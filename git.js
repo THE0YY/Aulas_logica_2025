4ec6b8deb1e62da4f56b1a56b3924e8c2ce7ac14
Theo Yusuke <theo.fontanin@aluno.senai.br>
2025-04-08 10:50:48 -0300

    branch

--- git.js
+++ git.js
@@ -1,5 +1,6 @@
 let comida = ['lasanha', 'pizza', 'sushi', 'hamburguer', 'batata frita']
 
 console.log(`todas as comididas: ${comida}`);
+console.log(`teste mesclar branch`);
 comida.sort()
 console.log('comidas ordenadas', comida);
